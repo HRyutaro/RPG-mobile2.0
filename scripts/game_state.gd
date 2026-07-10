a29d35d02f953a87e00c31311423d88547af872c
HRyutaro <erik.hakamada@gmail.com>
2026-07-10 17:28:07 -0300

    chore: setup projeto Godot 4.7 + GUT + GameState + sanity

---
+++ scripts/game_state.gd
@@ -0,0 +1,4 @@
+extends Node
+## Guarda estado entre cenas (substitui o PlayerPrefs do Unity).
+
+var classe_escolhida: int = 0 # CombatEnums.CharacterType
---
+++ test/test_sanity.gd
@@ -0,0 +1,4 @@
+extends GutTest
+
+func test_harness_roda():
+	assert_eq(1 + 1, 2)
